2e9c82d891198f966a9327c2e373ed4f02cf14a0
Om Chikane <chikaneom007@gmail.com>
2022-04-07 09:04:09 +0530

    Checks for the Player Options that are NoPLayladder or Snake

--- SankeLadder.js
+++ SankeLadder.js
@@ -1,5 +1,22 @@
 console.log("Welcome to Snake Ladder Simmulator");
 let startPosition = 0;
+let noPlay = 0;
+let ladder = 1;
+let snake = 2;
+
+let playerPosition = 0;
 
 let DiceRoll = (Math.floor(Math.random() * 10 ) % 6 ) + 1;
-console.log(DiceRoll); 
+let playerOption = Math.floor(Math.random() * 10 ) % 3 ; 
+
+switch(playerOption){
+    case ladder:
+        playerPosition += DiceRoll;
+        break;
+    case snake:
+        playerPosition -= DiceRoll;
+        break;
+    default:
+        break;
+}
+console.log("Position is :"+playerPosition)
